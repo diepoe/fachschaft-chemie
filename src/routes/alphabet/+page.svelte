9239ec7a1879f3824a485b55ef59b70970b884ac
Dietrich Poensgen <poedietrich@outlook.de>
2024-07-08 15:28:53 +0200

    add page that exclusevly displays the alphabet

---
+++ src/routes/alphabet/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Alpabet from '$lib/components/Alpabet.svelte';
+</script>
+
+<Alpabet
+	buttonColor="bg-white text-black hover:bg-blue-200 hover:text-black"
+	font="font-cmu"
+	selection=""
+/>
